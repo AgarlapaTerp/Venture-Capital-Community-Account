f17f9b6cc029e63667b4d1f87b6d346e621f3cf4
Ricardo Linares <ricardolinares2509@gmail.com>
2022-08-04 16:43:54 -0500

    created basic api

---
+++ vccommunity-api/app/models/index.js
@@ -0,0 +1,29 @@
+const dbConfig = require("../config/db.config.js");
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  operatorsAliases: false,
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle
+  }
+});
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.users = require("./users.model.js")(sequelize, Sequelize);
+    // columns: uuid(automatic), username, password, first, last, currentBalance
+db.companies = require("./companies.model.js")(sequelize, Sequelize);
+    // columns: uuid(automatic), company name, username, password, 
+db.userInvestments = require("./userInvestments.model.js")(sequelize, Sequelize);
+    // columns: uuid(automatic), userId, investmentAmount, companyId
+db.companyFunding = require("./companyFunding.model.js")(sequelize, Sequelize);
+    // columns: uuid(automatic), investmentAmount, userId, date
+
+
+
+module.exports = db;
